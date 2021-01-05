daf44bad71fef869dab7a8dd4d37fc9ed35ba029
Artem <Artemasdwasdw@mail.ru>
2021-01-05 18:04:32 +0300

    Add ITodo interface and update addHandler(App.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css'
 
 import { Navbar } from './components/Navbar'
 import { TodoForm } from './components/TodoForm';
 import { TodoList } from './components/TodoList';
+import { ITodo } from './components/Interface';
 
 function App() {
+  const [todos, setTodos] = useState<ITodo[]>([])
+
   const addHandler = (title: string) => {
-    
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false,
+    }
+    setTodos(prev => [newTodo, ...prev])
+    console.log(todos)
   }
 
   return (
