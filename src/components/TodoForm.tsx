2b36651c831e3a0747a46caec52a717beb9fa956
Artem <Artemasdwasdw@mail.ru>
2021-01-05 16:42:52 +0300

    Made work TodoForm

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,10 +1,25 @@
-import React from 'react' 
+import React, { useState } from 'react' 
 
 export const TodoForm: React.FC = () => {
+    const [title, setTitle] = useState('')
+
+    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.target.value)
+    }
+
+    const pressHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            console.log(title)
+            setTitle('')
+        }
+    }
+
     return (
         <div className="input-field mt2">
           <input 
-          value="" 
+          value={title}
+          onChange={event => changeHandler(event)}
+          onKeyPress={event => pressHandler(event)}
           id="title" 
           type="text" 
           className="validate" 
